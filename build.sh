42cfb034923186e05028e14e488baf64c1d21f9c
elagost <mail@elagost.com>
2022-11-12 11:04:54 -0600

    building without ffmpeg I guess works?

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+flatpak-builder .build1 ./org.lhowon.alephone.marathon1.yml
+flatpak-builder .build2 ./org.lhowon.alephone.marathon2.yml
+flatpak-builder .build3 ./org.lhowon.alephone.marathon3.yml
