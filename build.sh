c9290acc01b3fb22e32defa93219bf81c66ec385
elagost <mail@elagost.com>
2022-11-12 11:46:50 -0600

    nicer build script

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
-flatpak-builder .build1 ./org.lhowon.alephone.marathon1.yml
-flatpak-builder .build2 ./org.lhowon.alephone.marathon2.yml
-flatpak-builder .build3 ./org.lhowon.alephone.marathon3.yml
+#!/usr/bin/env bash
+# for a signed repo:
+# $ GPGKEY=<gpg key id> ./build.sh
+# for an unsigned repo:
+# $ ./build.sh
+
+if [[ $GPGKEY ]];then
+	for x in {1..3};do
+		flatpak-builder --gpg-sign=$GPGKEY --force-clean --repo=./.flatpakrepo .build$x ./org.lhowon.alephone.marathon$x.yml
+	done
+else
+	for x in {1..3};do
+		flatpak-builder --force-clean --repo=./.flatpakrepo .build$x ./org.lhowon.alephone.marathon$x.yml
+	done
+fi
