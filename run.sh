a13c5323675a55b97300e59f11fb1140ddcfecdf
elagost <mail@elagost.com>
2022-12-01 20:49:04 -0600

    quote the ls!

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z $(ls -A ~/.var/app/$FLATPAK_ID/data/gamedata/) ];then
+if [ -z "$(ls -A ~/.var/app/$FLATPAK_ID/data/gamedata/)" ];then
 	unzip /app/extra/gamedata.zip -d ~/.var/app/$FLATPAK_ID/data/gamedata/
 	mv ~/.var/app/$FLATPAK_ID/data/gamedata/*/* ~/.var/app/$FLATPAK_ID/data/gamedata/
 fi
