b5ff4d1bc281a40593217ddf0b7d525e3e4fa1ce
Adam Thiede <adamj@mailbox.org>
2022-12-01 20:33:16 -0600

    fix run script syntax and use bash since it's included

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-if [ -z $(ls -A ~/.var/app/$FLATPAK_ID/data/gamedata/)];then
+#!/bin/bash
+if [ -z $(ls -A ~/.var/app/$FLATPAK_ID/data/gamedata/) ];then
 	unzip /app/extra/gamedata.zip -d ~/.var/app/$FLATPAK_ID/data/gamedata/
 	mv ~/.var/app/$FLATPAK_ID/data/gamedata/*/* ~/.var/app/$FLATPAK_ID/data/gamedata/
 fi
